3339aad6a8fbf2376232b93b0eba36a57da2fd55
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-04-29 10:55:47 +0545

    🐞 fix: Refactor Clerk authentication in admin layout and page components

--- src/app/(pages)/admin/layout.tsx
+++ src/app/(pages)/admin/layout.tsx
@@ -1,12 +1,7 @@
-import { ClerkProvider } from "@clerk/nextjs";
 export default function AdminLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <>
-      <ClerkProvider>{children}</ClerkProvider>
-    </>
-  );
+  return <>{children}</>;
 }
--- src/app/(pages)/admin/page.tsx
+++ src/app/(pages)/admin/page.tsx
@@ -1,44 +1,4 @@
-"use client";
-
-import {
-  ClerkLoaded,
-  ClerkLoading,
-  SignedIn,
-  SignedOut,
-  SignInButton,
-} from "@clerk/nextjs";
-import Link from "next/link";
-import { Loader } from "lucide-react";
-import { Button } from "@/components/ui/button";
 const AdminPage = () => {
-  return (
-    <div>
-      <ClerkLoading>
-        <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
-      </ClerkLoading>
-      <ClerkLoaded>
-        <SignedIn>
-          <Link href="/dashboard">
-            <Button
-              size="lg"
-              className="bg-black dark:bg-white rounded-2xl w-fit text-white dark:text-black "
-            >
-              Go to Dashboard
-            </Button>
-          </Link>
-        </SignedIn>
-        <SignedOut>
-          <SignInButton mode="modal">
-            <Button
-              size="lg"
-              className="bg-black dark:bg-white rounded-2xl w-fit text-white dark:text-black "
-            >
-              Login
-            </Button>
-          </SignInButton>
-        </SignedOut>
-      </ClerkLoaded>
-    </div>
-  );
+  return <div>Dashboard Landing Page</div>;
 };
 export default AdminPage;
