82784c2e990e47604420c9f5e8a6d0221afc48d9
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-05-18 17:47:13 +0545

    ✨feature: Add GET and DELETE APIs for contacts

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,5 +1,6 @@
 import { db } from "@/db/db";
 import { NewContactSchema } from "@/schema/contact.zod";
+import { currentUser } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export const POST = async (req: Request) => {
@@ -29,6 +30,74 @@ export const POST = async (req: Request) => {
         message: "Message sent successfully!",
       },
       { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        status: 500,
+        message: "Internal Server Error",
+        error: JSON.stringify(error),
+      },
+      { status: 500 }
+    );
+  }
+};
+
+export const GET = async (req: Request) => {
+  try {
+    const user = await currentUser();
+    if (!user || !user.id || !(user.publicMetadata.role === "admin")) {
+      return NextResponse.json(
+        {
+          status: 401,
+          message: "Unauthorized Access!",
+        },
+        { status: 401 }
+      );
+    }
+    const contacts = await db.contact.findMany();
+    return NextResponse.json(
+      {
+        status: 200,
+        data: contacts,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        status: 500,
+        message: "Internal Server Error",
+        error: JSON.stringify(error),
+      },
+      { status: 500 }
+    );
+  }
+};
+export const DELETE = async (req: Request) => {
+  try {
+    const user = await currentUser();
+    if (!user || !user.id || !(user.publicMetadata.role === "admin")) {
+      return NextResponse.json(
+        {
+          status: 401,
+          message: "Unauthorized Access!",
+        },
+        { status: 401 }
+      );
+    }
+    const { id } = await req.json();
+    await db.contact.delete({
+      where: {
+        id,
+      },
+    });
+    return NextResponse.json(
+      {
+        status: 200,
+        message: "Contact deleted successfully",
+      },
+      { status: 200 }
     );
   } catch (error) {
     return NextResponse.json(
